7d11e364835fc2814ba4c42cda8333916e865dda
manu <manu.llanos@gmail.com>
2024-06-10 16:18:30 -0700

    update defaults and formatting

--- autopath/config.py
+++ autopath/config.py
@@ -3,44 +3,49 @@ import json
 import logging
 from inspect import signature
 
+
 class Config(object):
     """This class handles the config.json options to set up an AutoPath simulation.
 
     :param object: inherits from the base class
     :type object: object
     """
-    def __init__(self,
-                 VS_mode: bool = False,
-                 pdb_path: str = None,
-                 do_fix_pdb: bool = True,
-                 pocket_selection: str = 'protein and (around 3 resname UNK) and (not name H*)',
-                 temperature: float = 300,
-                 random_state: int = 42,
-                 run_preparation: bool = True,
-                 forcefield: list = None,
-                 lig_ff: str = 'espaloma',
-                 boxShape: str = 'dodecahedron',
-                 padding: float = 1.0,
-                 ionicStrength: float = 0.0,
-                 run_equilibration: bool = True,
-                 run_sMDpulling: bool = True,
-                 sMD_pulling_dist: float = 0.5, # nm
-                 sMD_time: int = 1, # ns
-                 sMD_steps_per_move: int = 250, # 1 ps
-                 sMD_pulling_force: float = 20000, # KJ/mol/nm2
-                 sMD_replicas: int = 5,
-                 sMD_autostop: bool = False,
-                 extract_milestones: bool = True,
-                 n_milestones: int = 10,
-                 run_relax: bool = True,
-                 relax_steps: int = 25000,
-                 run_metadynamics: bool = True,
-                 mMD_walkers: int = 10,
-                 mMD_bias_factor: int = 3,
-                 mMD_hill_height: float = 0.3, # Kcal/mol
-                 mMD_time: int = 1, # ns 
-                 ):
-        
+
+    def __init__(
+        self,
+        VS_mode: bool = False,
+        pdb_path: str = None,
+        do_fix_pdb: bool = True,
+        pocket_selection: str = "protein and (around 3 resname UNK) and (not name H*)",
+        temperature: float = 300,
+        random_state: int = 42,
+        run_preparation: bool = True,
+        forcefield: list = None,
+        lig_ff: str = "espaloma",
+        boxShape: str = "dodecahedron",
+        padding: float = 1.0,
+        ionicStrength: float = 0.0,
+        variants: dict = None,
+        run_equilibration: bool = True,
+        equilibration_scheme: str = "autopath/data/equilibration.json",
+        run_sMDpulling: bool = True,
+        sMD_pulling_dist: float = 0.5,  # nm
+        sMD_time: int = 1,  # ns
+        sMD_steps_per_move: int = 250,  # 1 ps
+        sMD_pulling_force: float = 20000,  # KJ/mol/nm2
+        sMD_replicas: int = 5,
+        sMD_autostop: bool = False,
+        extract_milestones: bool = True,
+        n_milestones: int = 10,
+        run_relax: bool = True,
+        relax_steps: int = 25000,
+        run_metadynamics: bool = True,
+        mMD_walkers: int = 10,
+        mMD_bias_factor: int = 3,
+        mMD_hill_height: float = 0.3,  # Kcal/mol
+        mMD_time: int = 1,  # ns
+    ):
+
         # General
         self.VS_mode = VS_mode
         self.pdb_path = pdb_path
@@ -56,17 +61,19 @@ class Config(object):
         self.boxShape = boxShape
         self.padding = padding
         self.ionicStrength = ionicStrength
+        self.variants = variants
 
         # Equilibration
         self.run_equilibration = run_equilibration
+        self.equilibration_scheme = equilibration_scheme
 
         # Steered MD
         self.run_sMDpulling = run_sMDpulling
-        self.sMD_time = sMD_time # ns
+        self.sMD_time = sMD_time  # ns
         self.sMD_replicas = sMD_replicas
         self.sMD_pulling_dist = sMD_pulling_dist
-        self.sMD_steps_per_move = sMD_steps_per_move # 1 ps
-        self.sMD_pulling_force = sMD_pulling_force # KJ/mol/nm2
+        self.sMD_steps_per_move = sMD_steps_per_move  # 1 ps
+        self.sMD_pulling_force = sMD_pulling_force  # KJ/mol/nm2
         self.sMD_autostop = sMD_autostop
 
         # Milestones
@@ -85,9 +92,9 @@ class Config(object):
         self.equilibration_checkpoint = False
         if VS_mode:
             self.equilibration_checkpoint = True
-            self.eq_checkpoint_cutoff = 0.3 # nm 
+            self.eq_checkpoint_cutoff = 0.3  # nm
             self.pulling_checkpoint = True
-    
+
     @classmethod
     def get_defaults_dict(cls):
         """Returns a dictionary with all the default values for the class attributes.
@@ -98,7 +105,7 @@ class Config(object):
         defaults = {}
         sig = signature(cls.__init__)
         for key in sig.parameters:
-            defaults[key] = sig.parameters[key].default 
+            defaults[key] = sig.parameters[key].default
         return defaults
 
     @classmethod
@@ -111,16 +118,16 @@ class Config(object):
         :return: instance of the Config class
         :rtype: Config
         """
-        logging.info('Loading settings from JSON file.')
+        logging.info("Loading settings from JSON file.")
 
         try:
             with open(config) as f:
                 config_data = json.load(f)
         except FileNotFoundError:
-            logging.error(f'Config file {config} not found.')
+            logging.error(f"Config file {config} not found.")
             raise
         except json.JSONDecodeError:
-            logging.error(f'Config file {config} is not valid JSON.')
+            logging.error(f"Config file {config} is not valid JSON.")
             raise
 
         # Flatten the nested dictionary structure
@@ -135,7 +142,7 @@ class Config(object):
             err_msg = "Unexpected keys in Config.from_config():" + os.linesep
             for key in bad_keys:
                 err_msg += f"  - {key}" + os.linesep
-            logging.error('Failed to load settings from JSON file.')
+            logging.error("Failed to load settings from JSON file.")
             raise ValueError(err_msg)
 
         p = cls(**flattened_config)
