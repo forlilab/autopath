08ed5846abeb5e909c0a9e37226d9766ba948f3e
manu <manu.llanos@gmail.com>
2024-08-12 12:15:28 -0700

    change the equilibration class to allow more flexibility

--- autopath/equilibration.py
+++ autopath/equilibration.py
@@ -2,12 +2,47 @@ import time
 import json
 import logging
 
+from dataclasses import dataclass
+from typing import List, Dict, Any
+
 from openmm import *
 from openmm.app import *
 import openmm.unit as openmmunit
-from openmm.app.amberprmtopfile import AmberPrmtopFile
 
 from autopath.utils import *
+
+
+@dataclass
+class EquilibrationStep:
+    name: str
+    force_constants: List[float]
+    npt_flag: bool
+    nsteps: int
+    stepsize: float
+
+
+def set_force_constants(
+    simulation,
+    components: List[str],
+    force_constants: List[float],
+    prev_constants: Dict[str, float],
+) -> None:
+    """Set the force constants for the given components."""
+    for component, force_constant in zip(components, force_constants):
+        if (
+            prev_constants[component] is None
+            or force_constant != prev_constants[component]
+        ):
+            simulation.context.setParameter(
+                f"k_{component}",
+                (
+                    force_constant
+                    * openmmunit.kilocalories_per_mole
+                    / openmmunit.angstroms**2
+                ),
+            )
+            prev_constants[component] = force_constant
+    return
 
 
 def warm_up_system(
@@ -18,7 +53,7 @@ def warm_up_system(
     Tstep: int = 5,
     timestep: float = 0.001,
     warming_steps: int = 100000,
-):
+) -> None:
     """
     Perform simulated annealing, slowly increasing the temperature
     to warm-up the system in the NVT ensemble.
@@ -48,76 +83,40 @@ def equilibrate_restrained_system(
     simulation,
     system,
     integrator,
-    equil_scheme: dict = None,
+    equil_scheme: Dict[str, Any],
     temp: int = 300,
     is_membrane: bool = False,
 ) -> None:
-    """Do restrained equilibration, releasing constraints
-    on protein and ligands and increasing timestep
-    """
+    """Perform restrained equilibration, adjusting force constants, timestep, and barostat as needed."""
 
-    # Initialize variables as None
-    k_lig_prev, k_prot_prev, npt_prev, stepsize_prev = None, None, None, None
+    components = equil_scheme["components"]
+    steps = [EquilibrationStep(**step) for step in equil_scheme["steps"]]
 
-    for step_name, params in equil_scheme.items():
-        k_lig = params["k_lig"]
-        k_prot = params["k_prot"]
-        npt_flag = params["npt_flag"]
-        nsteps = params["nsteps"]
-        stepsize = params["stepsize"]
+    prev_constants = {component: None for component in components}
+    npt_prev, stepsize_prev = None, None
 
+    for step in steps:
         logging.info(
-            f"Equilibration {step_name}: K_prot={k_prot} | K_lig={k_lig} | NPT={npt_flag} | n_steps={nsteps} | stepsize={stepsize}"
+            f"Equilibration {step.name}: force_constants={step.force_constants} | NPT={step.npt_flag} | n_steps={step.nsteps} | stepsize={step.stepsize} | components={components}"
         )
 
-        # Adjust force constant for the ligand if it has changed
-        if k_lig_prev is None or k_lig != k_lig_prev:
-            simulation.context.setParameter(
-                "k_lig",
-                (k_lig * openmmunit.kilocalories_per_mole / openmmunit.angstroms**2),
-            )
+        set_force_constants(
+            simulation, components, step.force_constants, prev_constants
+        )
 
-        # Adjust force constant for the protein if it has changed
-        if k_prot_prev is None or k_prot != k_prot_prev:
-            simulation.context.setParameter(
-                "k_prot",
-                (k_prot * openmmunit.kilocalories_per_mole / openmmunit.angstroms**2),
-            )
-
-        # Enable NPT if needed
-        if npt_flag != npt_prev and npt_flag:
-            logging.debug(f"Adding a Montecarlo Barostat to the system")
-            if is_membrane:
-                # Add the membrane barostat
-                barostat = MonteCarloMembraneBarostat(
-                    1 * openmmunit.atmosphere,
-                    0 * openmmunit.bar * openmmunit.nanometers,
-                    temp,
-                    MonteCarloMembraneBarostat.XYIsotropic,
-                    MonteCarloMembraneBarostat.ZFree,
-                    10,
-                )
-            else:
-                # Add a normal barostat
-                barostat = MonteCarloBarostat(1 * openmmunit.atmosphere, temp)
-
-            system.addForce(barostat)
+        if step.npt_flag != npt_prev and step.npt_flag:
+            add_barostat(system, temp, is_membrane)
             simulation.context.reinitialize(preserveState=True)
 
-        # Adjust the timestep if it has changed
-        if stepsize_prev is None or stepsize != stepsize_prev:
-            integrator.setStepSize(stepsize)
+        if stepsize_prev is None or step.stepsize != stepsize_prev:
+            integrator.setStepSize(step.stepsize)
             simulation.context.reinitialize(preserveState=True)
             logging.debug(f"Stepsize set to {integrator.getStepSize()}")
 
-        # Run the simulation for the specified number of steps
-        simulation.step(nsteps)
+        simulation.step(step.nsteps)
 
-        # Update previous values
-        k_lig_prev = k_lig
-        k_prot_prev = k_prot
-        npt_prev = npt_flag
-        stepsize_prev = stepsize
+        npt_prev = step.npt_flag
+        stepsize_prev = step.stepsize
 
     return
 
@@ -125,49 +124,57 @@ def equilibrate_restrained_system(
 class Equilibration:
     def __init__(
         self,
-        system_file: str = None,
-        prmtop_file: str = None,
+        topology: str = None,
+        system: str = None,
         out_dir: str = "equilibration",
         lig_name: str = "UNK",
-        equilibration_scheme: str = "autopath/data/equilibration.json",
+        equilibration_fname: str = "autopath/data/equilibration.json",
         warm_up_steps: int = 100000,
         temperature: float = 300,
         timestep: float = 0.004,
         is_membrane: bool = False,
+        lipid_type: str = "POPC",
     ) -> None:
 
-        self.system = load_system(system_file)
-        prmtop = AmberPrmtopFile(prmtop_file)
-        self.topology = prmtop.topology
+        self.system = system
+        self.topology = topology
         self.lig_name = lig_name
 
         self.out_dir = out_dir
         os.makedirs(out_dir, exist_ok=True)
 
         self.is_membrane = is_membrane
+        self.lipid_type = lipid_type
+        # TODO lipid type and residue name is not the same i.e. POPC/POP. We need a mapping dict
+
         self.temperature = temperature * openmmunit.kelvin
         self.timestep = timestep * openmmunit.picoseconds
         self.warm_up_steps = warm_up_steps
 
         self.platform = select_platform("fastest")
 
-        try:
-            logging.info("Loading equilibration protocol from JSON file.")
-            with open(equilibration_scheme) as f:
-                self.equilibration_scheme = json.load(f)
-        except FileNotFoundError:
-            logging.error(f"{equilibration_scheme} not found.")
-            raise
-        except json.JSONDecodeError:
-            logging.error(f"{equilibration_scheme} is not valid JSON.")
-            raise
+        self.equilibration_scheme = self.from_json(equilibration_fname)
 
         equilibration_steps = sum(
-            [v["nsteps"] for k, v in self.equilibration_scheme.items()]
+            [v["nsteps"] for v in self.equilibration_scheme["steps"]]
         )
         self.total_steps = warm_up_steps + equilibration_steps
 
         return
+
+    def from_json(self, equilibration_fname: str = None) -> dict:
+        try:
+            logging.info("Loading equilibration protocol from JSON file.")
+            with open(equilibration_fname) as f:
+                equilibration_scheme = json.load(f)
+        except FileNotFoundError:
+            logging.error(f"{equilibration_fname} not found.")
+            raise
+        except json.JSONDecodeError:
+            logging.error(f"{equilibration_fname} is not valid JSON.")
+            raise
+
+        return equilibration_scheme
 
     def run(self, pdb_file: str = None) -> None:
 
@@ -180,9 +187,10 @@ class Equilibration:
         # integrator.setRandomNumberSeed(seed)
         integrator.setConstraintTolerance(0.00001)
 
-        simulation = Simulation(self.topology, self.system, integrator, self.platform)
+        pdb = PDBFile(pdb_file)
+        initial_positions = pdb.positions
 
-        initial_positions = PDBFile(pdb_file).positions
+        simulation = Simulation(self.topology, self.system, integrator, self.platform)
         simulation.context.setPositions(initial_positions)
 
         add_reporters(
@@ -204,7 +212,7 @@ class Equilibration:
             self.topology,
             prot_ha_idx,
             restraint_force=5,
-            force_name="k_prot",
+            force_name="k_protein",
             force_group=12,
         )
 
@@ -219,9 +227,22 @@ class Equilibration:
             self.topology,
             lig_ha_idx,
             restraint_force=5,
-            force_name="k_lig",
+            force_name="k_ligand",
             force_group=13,
         )
+
+        if self.is_membrane:
+            logging.info("Adding harmonic restraints to the membrane..")
+            lipid_ha_idx, lipid_ha_names = get_ligand_ha(self.topology, "POP")
+            add_harmonic_restraints(
+                self.system,
+                initial_positions,
+                self.topology,
+                lipid_ha_idx,
+                restraint_force=10,
+                force_name="k_membrane",
+                force_group=14,
+            )
 
         logging.info("Minimizing..")
         simulation.minimizeEnergy()
@@ -239,7 +260,8 @@ class Equilibration:
             self.is_membrane,
         )
 
-        # Remove both protein and ligand force restraints
+        # TODO forces should be removed by name. OpenMM behavior is weird with that
+        # Remove protein and ligand force restraints
         simulation.context.getSystem().removeForce(
             simulation.context.getSystem().getNumForces() - 3
         )
@@ -261,4 +283,4 @@ class Equilibration:
             f"Restrained equilibration completed in {simulation_time/60:.2f} min."
         )
 
-        return None
+        return self.system
