10c13d9cae9b23f227a0567ae2548177459e529c
manu <manu.llanos@gmail.com>
2024-08-12 11:51:49 -0700

    small fixes

--- autopath/__init__.py
+++ autopath/__init__.py
@@ -6,7 +6,6 @@
 
 __version__ = "0.1.0"
 
-# from .autopath_core import AutoPath
 from .equilibration import Equilibration
 from .preparation import SystemPreparation
 from .steered_md import SteeredMD
@@ -15,8 +14,10 @@ from .relax_md import RelaxMD
 from .vanilla_md import VanillaMD
 
 __all__ = [
-        #    'AutoPath',
-           'Equilibration', 'SystemPreparation',
-           'SteeredMD', 'MetadynamicsMD',
-           'RelaxMD','VanillaMD'
-            ]
+    "Equilibration",
+    "SystemPreparation",
+    "SteeredMD",
+    "MetadynamicsMD",
+    "RelaxMD",
+    "VanillaMD",
+]
