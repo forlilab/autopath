c27a46168d869b7861a99408b387874987fbaa99
Manuel Llanos <manu.llanos@gmail.com>
2024-10-03 12:46:43 -0700

    enable backward mode for sMD

--- autopath/steered_md.py
+++ autopath/steered_md.py
@@ -52,97 +52,67 @@ class SteeredMD:
         pulling_force: int = 1000,
         replicas: int = 5,
         checkpoint_file: str = None,
+        do_backwards: bool = False,
     ):
 
-        start_time = time.monotonic()
+        simulation_start_time = time.monotonic()
+
+        sys_name = self.out_dir.split("/")[0]
 
         # Calculate the number of steps required
         sMD_steps = math.ceil(sMD_time / self.timestep * 1000.0)  # 250.000 1ns at 4fs
         sMD_moves = int(sMD_steps / steps_per_move)
 
-        self.fc_pull = (
-            pulling_force * openmmunit.kilojoules_per_mole / openmmunit.nanometer**2
-        )
+        dx_per_move = (displacement / sMD_moves) * openmmunit.nanometers
 
-        logging.info("Setting up the integrator..")
+        velocity = displacement / sMD_time        
+        logging.info(
+            f"Running {sMD_time} ns in {sMD_moves} moves of {steps_per_move} steps each.."
+        )
+        logging.info(f"Pulling rate i.e. velocity will be {velocity:.2f} nm/ns")
+
+        self.fc_pull =  pulling_force * openmmunit.kilojoules_per_mole / openmmunit.nanometer**2
+
+        logging.debug("Setting up the integrator..")
         integrator = LangevinMiddleIntegrator(
             self.temperature, 1 / openmmunit.picoseconds, self.timestep
         )
-        # integrator.setRandomNumberSeed(int(rep_idx))
 
         # Setting Simulation object and loading the checkpoint
         simulation = Simulation(self.topology, self.system, integrator, self.platform)
 
-        if checkpoint_file is not None:
-            logging.info("Loading simulation checkpoint..")
-            simulation.loadCheckpoint(checkpoint_file)
+        def run_single_direction(rep_idx, direction="", dx_per_move=dx_per_move):
 
-        # Add harmonic positional restraints to protein CA
-        input_positions = simulation.context.getState(getPositions=True).getPositions()
-        
-        if self.restrained_atoms is not None:
-            add_harmonic_restraints(
-                self.system,
-                input_positions,
-                self.topology,
-                self.restrained_atoms,
-                10,
-                "k_CA",
-                14,
-            )
-
-        startdist = get_COG_dist(simulation, self.ligand_atoms, self.pocket_atoms)
-        initial_r0 = startdist * openmmunit.nanometers
-        logging.info(f"Initial COM distance is {startdist:.2f} nm")
-
-        velocity = displacement / sMD_time
-        logging.info(f"Pulling rate i.e. velocity will be {velocity:.2f} nm/ns")
-
-        dx_per_move = (displacement / sMD_moves) * openmmunit.nanometers
-
-        logging.info(
-            f"Running {sMD_time} ns in {sMD_moves} moves of {steps_per_move} steps each.."
-        )
-
-        add_COM_force(
-            self.system, self.ligand_atoms, self.pocket_atoms, self.fc_pull, initial_r0
-        )
-        simulation.context.reinitialize(preserveState=True)
-        simulation.context.setTime(0)  # reset simulation time
-        simulation.context.setParameter("r0", initial_r0)
-
-        for rep_idx in range(1, replicas + 1):
-
-            logging.info(f"Replica {rep_idx}/{replicas}")
-
-            if checkpoint_file is not None:
-                logging.info("Loading simulation checkpoint..")
-                simulation.loadCheckpoint(checkpoint_file)
-
-            simulation.context.setParameter("r0", initial_r0)
+            logging.info(f"Replica {rep_idx}/{replicas} {direction}")
 
             add_reporters(
                 simulation,
                 self.out_dir,
-                f"sMD_{rep_idx}",
+                f"sMD_{rep_idx}_{direction}",
                 sMD_steps,
-                steps_per_move * 2,
-            )  # dont save too often
+                steps_per_move * 2, #CHECK
+            )  # don't save too often
+
+
+            startdist = get_COM_dist(simulation, self.ligand_atoms, self.pocket_atoms)
+            initial_r0 = startdist * openmmunit.nanometers
+            logging.info(f"Initial COM distance is {startdist:.2f} nm")
+
+            simulation.context.setTime(0)  # reset simulation time
+            simulation.context.setParameter("r0", initial_r0)
 
             # Initializing work
             work_val_old = openmmunit.Quantity(
                 value=0, unit=openmmunit.kilojoules_per_mole
             )
 
-            f = open(f"{self.out_dir}/sMD_log_{rep_idx}.dat", "a")
+            f = open(f"{self.out_dir}/sMD_log_{rep_idx}_{direction}.dat", "a")
             for i in range(sMD_moves):
 
                 # Get COM distance
-                current_dist = get_COG_dist(
+                current_dist = get_COM_dist(
                     simulation, self.ligand_atoms, self.pocket_atoms
-                )
-                current_dist = current_dist * openmmunit.nanometers
-                # logging.info(f'Current distance is {current_dist}')
+                ) * openmmunit.nanometers
 
                 # Get radius of starting point and end point
                 r_current = initial_r0 + float(i + 1) * dx_per_move
@@ -156,8 +126,6 @@ class SteeredMD:
                 simulation.step(steps_per_move)
 
                 # Calculate work for difference in potential energy in transition
-                # W = EK_end - EK_start
-                # EK = 0.5 * k * x^2
                 spr_energy_end = 0.5 * -self.fc_pull * (current_dist - r_current) ** 2
                 spr_energy_start = 0.5 * -self.fc_pull * (current_dist - r_start) ** 2
                 work_val = work_val_old + spr_energy_end - spr_energy_start
@@ -169,25 +137,75 @@ class SteeredMD:
                 )
             f.close()
 
-            # _print_current_forces(system)
+            # Get COM distance
+            final_dist = get_COM_dist(
+                simulation, self.ligand_atoms, self.pocket_atoms
+                )
+            logging.info(f"Final COM distance is {final_dist:.2f} nm")
 
-            # Save state in PDB file
             final_positions = simulation.context.getState(
                 getPositions=True
             ).getPositions()
             save_pdb(
                 self.topology,
                 final_positions,
-                f"{self.out_dir}/steeredMD_{rep_idx}.pdb",
+                f"{self.out_dir}/steeredMD_{rep_idx}_{direction}.pdb",
             )
 
-            # Get statistics related to the pooling and plot them
-            files = glob(f"{self.out_dir}/*.dat")
-            data = extract_sMD_statistics(files)
-            sys_name = self.out_dir.split("/")[0]
-            plot_sMD_statistics(data, sys_name, self.out_dir)
+            return None
+        
+        # Outer loop over replicas
+        for rep_idx in range(1, replicas + 1):
+            replica_start_time = time.monotonic()
+            logging.info(f"Running forward pulling for replica {rep_idx}/{replicas}.")
 
-        simulation_time = time.monotonic() - start_time
+            if checkpoint_file is not None:
+                logging.info("Loading simulation checkpoint..")
+                simulation.loadCheckpoint(checkpoint_file)
+
+            # Add harmonic positional restraints to protein CA
+            input_positions = simulation.context.getState(getPositions=True).getPositions()
+
+            if self.restrained_atoms is not None:
+                add_harmonic_restraints(
+                    self.system,
+                    input_positions,
+                    self.topology,
+                    self.restrained_atoms,
+                    10,
+                    "k_CA",
+                    14,
+                )
+
+            # Add COM force with arbitrary initial r0, then run_single_direction will set it properly
+            add_COM_force(
+                self.system, self.ligand_atoms, self.pocket_atoms, self.fc_pull, 0 * openmmunit.nanometers
+            )
+            simulation.context.reinitialize(preserveState=True)
+
+            # Forward pulling (out of the pocket)
+            run_single_direction(rep_idx, direction="forward", dx_per_move=dx_per_move)
+
+            # Get statistics related to the pooling and plot them
+            files_f = glob(f"{self.out_dir}/sMD_log_*_forward.dat")
+            plot_sMD_statistics(files_f, f'{sys_name}_forward', self.out_dir)
+
+            if do_backwards:
+                logging.info(f"Running backward pulling for replica {rep_idx}/{replicas}.")
+
+                # Reverse displacement for backwards simulation
+                run_single_direction(rep_idx, direction="backward", dx_per_move=-dx_per_move)
+
+                # Get statistics related to the pooling and plot them
+                files_b = glob(f"{self.out_dir}/sMD_log_*_backward.dat")
+                plot_sMD_statistics(files_b, f'{sys_name}_backward', self.out_dir)
+
+            replica_time = time.monotonic() - replica_start_time
+            logging.info(
+                f"Finished replica {rep_idx}/{replicas} {'with backwards pulling' if do_backwards else ''} in {replica_time/60:.2f} min."
+            )
+            
+        simulation_time = time.monotonic() - simulation_start_time
         logging.info(
-            f"Finished {replicas} replicas of sMD in {simulation_time/60:.2f} min."
+            f"Finished {replicas} replicas of sMD {'with backwards pulling' if do_backwards else ''} in {simulation_time/60:.2f} min."
         )
