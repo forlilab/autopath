eb812cf2f6faa62e0ce5ec211913df3ef4e8b48d
Manuel Llanos <manu.llanos@gmail.com>
2024-10-12 16:30:18 -0700

    Add ClusterTrajectories class for trajectory clustering and analysis

--- autopath/__init__.py
+++ autopath/__init__.py
@@ -12,6 +12,7 @@ from .steered_md import SteeredMD
 from .metadynamics import MetadynamicsMD
 from .relax_md import RelaxMD
 from .vanilla_md import VanillaMD
+from .clustering import ClusterTrajectories
 
 __all__ = [
     "Equilibration",
@@ -20,4 +21,5 @@ __all__ = [
     "MetadynamicsMD",
     "RelaxMD",
     "VanillaMD",
+    "ClusterTrajectories",
 ]
