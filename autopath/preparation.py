a073d4be5167caf822f96d0d26d94e317f50f884
Manuel Llanos <manu.llanos@gmail.com>
2024-11-04 15:17:25 -0800

    small fix on the padding flag

--- autopath/preparation.py
+++ autopath/preparation.py
@@ -63,14 +63,14 @@ class SystemPreparation:
 
         self.hydrogenMass = hydrogenMass * openmmunit.amu  # Use HMR
         self.boxShape = boxShape  # cube, dodecahedron
-        
+
         if num_solvent is not None and padding is not None:
             logging.error(
                 "The arguments 'num_solvent' and 'padding' are incompatible. Please specify only one."
             )
             exit(1)
-
-        self.padding = padding * openmmunit.nanometers
+        if padding is not None:
+            self.padding = padding * openmmunit.nanometers
         self.num_solvent = num_solvent
 
         self.ionicStrength = ionicStrength * openmmunit.molar
