719cb228f22968dc4d06c0761a898c7e87fd8d81
Manuel Llanos <manu.llanos@gmail.com>
2024-11-08 14:21:00 -0800

    implement MoSAIC feature selection

--- autopath/clustering.py
+++ autopath/clustering.py
@@ -15,15 +15,18 @@ from sklearn.decomposition import PCA
 from scipy.spatial import KDTree
 
 import MDAnalysis as mda
-import pyemma
+# import pyemma
 
 from autopath.utils import save_model, load_model
-import umap
 from sklearn.metrics import silhouette_score
+
+import mosaic
+import umap
 
 class ClusterTrajectories:
     def __init__(self, 
-                embedding_model:str='umap',
+                mosaic_top_clusters:int=3,
+                embedding_model:str='vamp',
                 embedding_dim:Union[int, float]=2, 
                 embedding_lagtime:int=20,
                 clustering_model:str='kmeans',
@@ -33,7 +36,8 @@ class ClusterTrajectories:
                 out_dir:str='milestones',
                 seed:int=42,
                 ):
-
+        
+        self.mosaic_top_clusters = mosaic_top_clusters
         self.embedding_model = embedding_model.upper()
         self.embedding_dim = embedding_dim
         self.embedding_lagtime = embedding_lagtime
@@ -64,7 +68,89 @@ class ClusterTrajectories:
             logging.error(f'No data found at {data_fname}')
             exit(1)
             return None
-        
+    
+    ### MoSAIC analysis ###
+    @staticmethod
+    def mosaic_clustering(X, similarity_metric='correlation', 
+                        clustering_mode='CPM', weighted=True, 
+                        resolution_parameter=0.5):
+
+        # Merge all trajs together
+        X = np.concatenate(X, axis=0)
+
+        sim = mosaic.Similarity(
+            metric=similarity_metric,  # or 'NMI', 'GY', 'JSD'
+        )
+        sim.fit(X)
+        correlation_matrix = sim.matrix_
+
+        # Cluster the correlation matrix
+        clustering = mosaic.Clustering(
+            mode=clustering_mode,  # or 'modularity
+            weighted=weighted,
+            resolution_parameter=resolution_parameter,
+        )
+        clustering.fit(correlation_matrix)
+
+        clusters = clustering.clusters_
+        clustered_X = clustering.matrix_
+
+        return correlation_matrix, clusters, clustered_X
+    
+    @staticmethod
+    def plot_clusters(correlation_matrix, clusters, out_dir):
+
+        idxs = np.argsort(
+            [len(cluster) for cluster in clusters],
+        )[::-1]
+        clusters_sorted = clusters[idxs]
+        clusters_sorted_flattened = np.concatenate(clusters[idxs])
+
+        # sort the matrix accordingly
+        matrix_sorted = correlation_matrix[
+            np.ix_(clusters_sorted_flattened, clusters_sorted_flattened)
+        ]
+
+        ticks = np.cumsum([len(cluster) for cluster in clusters[idxs]])
+        ticks = [0, *ticks[:-1]]  # ticks start with 0 
+
+        # Perform the same plot again, but with sorted clusters
+        fig, ax = plt.subplots()
+        im = ax.pcolormesh(
+            matrix_sorted,
+            snap=True,
+            vmin=0,
+            vmax=1,
+        )
+        ax.invert_yaxis()  # origin to the upper left
+        ax.set_aspect('equal')  # 1:1 ratio
+        ax.set_xticks(ticks[:4])  # we focus only on the first three clusters
+        ax.set_yticks(ticks[:4])
+        ax.set_xticklabels(np.arange(4)+1)
+        ax.set_yticklabels(np.arange(4)+1)
+        ax.set_xlabel('clusters')
+        ax.set_ylabel('clusters')
+        ax.grid(True)
+        plt.colorbar(im, label=r'$|\rho|$')
+        plt.show()
+        plt.savefig(f"{out_dir}/mosaic_clustering.png")
+        plt.close()
+        return clusters_sorted
+    
+    @staticmethod
+    def filter_data_mosaic(data, clusters_sorted, top_clusters=3):
+        features = set()
+        for cluster in range(top_clusters):
+            for feats in clusters_sorted[cluster]:
+                features.add(feats)
+
+        print(f"Number of unique features: {len(features)}")
+
+        cols = list(features)
+        filtered_data = data[:,:,cols]
+
+        return filtered_data
+
     def fit_tica_model(self, data):
 
         # Scaling parameter
@@ -100,16 +186,16 @@ class ClusterTrajectories:
 
         return vamp_model
 
-    def fit_kvad_model(self, data):
+    def fit_kvad_model(self, data, kernel_width:float=0.75, epsilon:float=1e-6):
 
-        kvad_estimator = KVAD(kernel=GaussianKernel(0.5),
-            lagtime=self.embedding_lagtime, epsilon=1e-5, dim=self.embedding_dim,
+        kvad_estimator = KVAD(kernel=GaussianKernel(kernel_width),
+            lagtime=self.embedding_lagtime, epsilon=epsilon, dim=self.embedding_dim,
             )
         kvad_model = kvad_estimator.fit(data).fetch_model()
 
         return kvad_model
     
-    def fit_umap_model(self, data, n_neighbors=50, min_dist=0.5, metric='euclidean'):
+    def fit_umap_model(self, data, n_neighbors=15, min_dist=0.5, metric='euclidean'):
 
         X = np.concatenate(data, axis=0)
         umap_estimator = umap.UMAP(n_components=self.embedding_dim, 
@@ -127,7 +213,7 @@ class ClusterTrajectories:
             X = data
         else:
             X = np.concatenate(data, axis=0)
-        print(f'shape of X: {X.shape}')
+
         pca_estimator = PCA(n_components=self.embedding_dim)
         pca_model = pca_estimator.fit(X)
 
@@ -251,17 +337,18 @@ class ClusterTrajectories:
         #     # pyemma.plots.plot_density(*projection_concatenated.T, alpha=0.2)
             if labels is not None and centroids is not None:
                 sns.scatterplot(data=df, x='comp 1', y='comp 2', s=5, alpha=0.2, hue='cluster', palette='Set1')
-                sns.scatterplot(data=centroids_df,  x='comp 1', y='comp 2', s=15, c='black', marker='X')#, color='black', label='Centroids')
+                sns.scatterplot(data=centroids_df,  x='comp 1', y='comp 2', s=25, c='black', marker='X')#, color='black', label='Centroids')
             else:
                 sns.scatterplot(df, s=5, alpha=0.2)
             plt.xlabel('comp 1'); plt.ylabel('comp 2')
 
         elif projection_concatenated.shape[1] > 2:    
-            df = df.iloc[:, :5]  # Only plot the first 5 components
+            _df = df.iloc[:, :5]  # Only plot the first 5 components
+            _df['cluster'] = df['cluster']
             if labels is not None and centroids is not None:
-                g = sns.PairGrid(df, hue='cluster', corner=True, palette='Set1')
+                g = sns.PairGrid(_df, hue='cluster', corner=True, palette='Set1')
             else:
-                g = sns.PairGrid(df, corner=True)
+                g = sns.PairGrid(_df, corner=True)
             g.map_lower(sns.scatterplot, alpha=0.2, s=5)
             g.map_diag(sns.kdeplot, hue=None, color=".3")
             # g.map_upper(sns.kdeplot)
@@ -455,7 +542,6 @@ class ClusterTrajectories:
         plt.close()
 
         return df
-
  
     def run(self,
             data_fname:str=None,
@@ -466,6 +552,11 @@ class ClusterTrajectories:
         data = self._get_raw_data(data_fname)
         self.prmtop_file = prmtop_file
         self.traj_list = traj_list
+
+        if self.mosaic_top_clusters is not None:
+            correlation_matrix, clusters, clustered_X = self.mosaic_clustering(data)
+            clusters_sorted = self.plot_clusters(correlation_matrix, clusters, self.out_dir)
+            data = self.filter_data_mosaic(data, clusters_sorted, top_clusters=self.mosaic_top_clusters)
 
         fitted_embedding_model, projection = self.fit_embedding_model(data)
         save_model(fitted_embedding_model, f'{self.out_dir}/{self.embedding_model}_model_{self.embedding_dim}d_{self.embedding_lagtime}lag.pkl')
