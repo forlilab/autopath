fe8d5ec195c1eb293ac6b04406cb2678ebd3198c
manu <manu.llanos@gmail.com>
2025-06-25 20:13:50 -0700

    change clustering strategy

--- autopath/autopath_core.py
+++ autopath/autopath_core.py
@@ -304,68 +304,74 @@ class AutoPath:
         ##############################################################################################
         ###################################### Extract Milestones ####################################
         ##############################################################################################
-
         
         if self.extract_milestones:
-            from sklearn.decomposition import PCA
-            from deeptime.clustering import KMeans
+            from deeptime.clustering import RegularSpace
+
+            os.makedirs(f"{sys_name}/milestones/pdbs", exist_ok=True)
 
             sMD_trajs = glob(f"{sys_name}/sMD/*_aligned.dcd")
             u_all = mda.Universe(prmtop_file, sMD_trajs)
+
             # use the same pocket selection as in the equilibration, but create a new atomgroup for this Universe
             pocket_atoms = u_all.select_atoms(f'index {" ".join(map(str, pocket_atom_indices))}')
             ligand_atoms = u_all.select_atoms(f'index {" ".join(map(str, ligand_atoms_indices))}')
 
-            pocket_atom_indices = [atom.index for atom in pocket_atoms]
+            # pocket_atom_indices = [atom.index for atom in pocket_atoms]
             pocket_residues = [f"{atom.resname}_{atom.resid}" for atom in pocket_atoms]
+            logging.info(f"Pocket residues are: {', '.join(set(pocket_residues))}")
             pocket_full_names = [f"{atom.resname}_{atom.resid}_{atom.index}" for atom in pocket_atoms]
             # logging.info(f"Pocket atoms are: {', '.join(set(pocket_full_names))}")
-            logging.info(f"Pocket residues are: {', '.join(set(pocket_residues))}")
 
-            # Calculate distance matrix for all frames
-            # Each frame is represented by a vector of distances between all pocket atoms and all ligand atoms
-            data_array = np.zeros((len(u_all.trajectory) ,(len(pocket_atoms)*len(ligand_atoms))))
-            for i, ts in enumerate(u_all.trajectory):
-                distances = distance_array(pocket_atoms, ligand_atoms, 
-                                            result=np.ndarray((len(pocket_atoms), len(ligand_atoms))))
-                data_array[i] = distances.flatten()
-            data_array = np.array(data_array)
+            coms = calculate_com_distance(u_all, ligand_atoms, pocket_atoms, wrap=False)
+            rmsd = compute_rmsd(u_all, u_all, alig_select=f"resname {lig_resname} and (not name H*)",
+                                groupselections={'ligand': f"resname {lig_resname} and (not name H*)"},
+                                                out_dir=f"{sys_name}/milestones/pdbs")
+            rmsd['COM'] = coms
+            X = rmsd[['RMSD_ligand', 'COM']].values
 
-            # do pca reduction
-            pca = PCA(n_components=2)  # retain 95% of variance
-            data_array_reduced = pca.fit_transform(data_array)
-            logging.info(f'Number of PCA components: {pca.n_components_}')
-            logging.info(f'Variance explained by PCA: {np.sum(pca.explained_variance_ratio_):.2f}')
-           
-            # cluster_estimator = RegularSpace(dmin=1.0, max_centers=10)
-            cluster_estimator = KMeans(n_clusters=self.n_milestones-1) # -1 because we will add the equilibrated system as cluster 0
-            fitted_model = cluster_estimator.fit(data_array_reduced).fetch_model()
+            # cluster_estimator = KMeans(n_clusters=5)
+            cluster_estimator = RegularSpace(dmin=3, max_centers=self.n_milestones)
+            fitted_model = cluster_estimator.fit(X).fetch_model()
             cluster_centers = fitted_model.cluster_centers
+            labels = fitted_model.transform(X)
 
-            closest_frames = match_cluster_centroids(data_array_reduced, cluster_centers) 
+            closest_frames = match_cluster_centroids(X, cluster_centers) 
 
-            for i, idx in enumerate(closest_frames):
-                dist = np.linalg.norm(data_array_reduced[idx] - cluster_centers[i])
-                logging.info(f"Cluster {i}: Closest frame is {idx} (distance = {dist:.6f})")
+            # for i, idx in enumerate(closest_frames):
+            #     dist = np.linalg.norm(X[idx] - cluster_centers[i])
+            #     logging.info(f"Cluster {i}: Closest frame is {idx} (distance = {dist:.3f})")
+
+            # Plot the clustering results
+            plt.figure(figsize=(6, 5))
+            sns.scatterplot(x=rmsd['RMSD_ligand'], y=rmsd['coms'], hue=labels, palette='viridis')
+            plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], color='red', marker='x', s=100, label='Cluster Centers')
+            plt.xlabel('RMSD (A)'); plt.ylabel('COM Distance (A)')
+            plt.title(f"{sys_name} sMD clustering")
+            plt.legend()
+            plt.savefig(f"{sys_name}/milestones/pdbs/milestones_clustering_plot.png")
+            plt.close()
 
             # Write each representative frame to a PDB
-            os.makedirs(f"{sys_name}/milestones/pdbs", exist_ok=True)
             u_all.trajectory[0]  # reset
             for i, frame_index in enumerate(closest_frames):
                 u_all.trajectory[frame_index]
                 with mda.Writer(os.path.join(f"{sys_name}/milestones/pdbs", f"milestone_{i+1}_frame_{frame_index}.pdb"), u_all.atoms.n_atoms) as W:
                     W.write(u_all.atoms)
 
-            #make sure the equilibrated system is in the milestones folder
-            u_eq.trajectory[-1] 
-            with mda.Writer(os.path.join(f"{sys_name}/milestones/pdbs", f"milestone_0_frame_0.pdb"), u_eq.atoms.n_atoms) as W:
-                W.write(u_eq.atoms)
+            # Add the equilibrated system is in the milestones folder
+            # u_eq.trajectory[-1] 
+            # with mda.Writer(os.path.join(f"{sys_name}/milestones/pdbs", f"milestone_0_frame_0.pdb"), u_eq.atoms.n_atoms) as W:
+            #     W.write(u_eq.atoms)
 
         ##############################################################################################
         ##################################### Metadynamics simulations ###############################
         ##############################################################################################
 
         if self.run_metadynamics:
+
+            min_com = eq_com * 0.75
+            max_com = self.sMD_pulling_dist
 
             milestones = glob(f'{sys_name}/milestones/pdbs/milestone_*.pdb')           
             if len(milestones) == 0:
@@ -383,9 +389,6 @@ class AutoPath:
                 temp=self.temperature,
             )
 
-            min_com = eq_com * 0.75
-            max_com = self.sMD_pulling_dist
-            
             WTMetaD = MetadynamicsMD(
                 topology=topology,
                 ligand_atoms=ligand_atoms_indices,
