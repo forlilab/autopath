a768bb9d6c5c574b9f85673da9e83038b6723749
manu <manu.llanos@gmail.com>
2025-10-21 10:00:24 -0700

    update equilibration example

--- example/equilibration/ap_equilibration.py
+++ example/equilibration/ap_equilibration.py
@@ -3,9 +3,10 @@ import logging
 import argparse
 
 import MDAnalysis as mda
+import mdtraj as md
 
 from autopath import SystemPreparation, Equilibration, SteeredMD
-from autopath.analysis import plot_rmsd, plot_atomic_rmsf
+from autopath.analysis import plot_atomic_rmsf
 from autopath.utils import fix_pdb, save_pdb, load_system, align_trajectory, calculate_com_distance, compute_rmsd
 from openmm.app import PDBFile
 
@@ -104,7 +105,7 @@ def main():
     prepare_system = SystemPreparation(
             out_dir=sys_name,
             boxShape="dodecahedron",
-            padding=1.0,
+            padding=1.2,
             lig_ff="espaloma",
             ionicStrength=0.15,
             is_membrane=False,
@@ -140,93 +141,37 @@ def main():
     
     # If you re-run the script and the system is equilibrated prepared comment the following line
     system_eq = equilibration.run(pdb_file=system_pdb_file, run_id=sys_name)
+        
+    ########################################################################################
+    ###################################### Post-processing #################################
+    ########################################################################################
 
     system_prmtop = f"{sys_name}/system.prmtop"
-    equilibrated_traj = f"{sys_name}/equilibration/trajectory_equilibration_{sys_name}.dcd"
-    # Wrap, align and save the clean trajectory
-    align_trajectory(
-        system_prmtop,
-        equilibrated_traj,
-        out_fname=f"{sys_name}/equilibration/{sys_name}_aligned.dcd",
-        strip_mask=None #you can dry the traj or remove garbage
-    )
-    equilibrated_traj = f"{sys_name}/equilibration/{sys_name}_aligned.dcd"
-    # Calculate RMSD and RMSF of the ligand
-    u_eq = mda.Universe(system_prmtop, equilibrated_traj, in_memory=True)
+    equilibrated_traj = f"{sys_name}/equilibration/equilibration_{sys_name}.dcd"
     
+    # Wrap, align and save the clean trajectory
+    traj = md.load(equilibrated_traj, top=system_prmtop)
+    traj = traj.center_coordinates()
+    traj = traj.image_molecules()
+    try: # if there's no protein
+        backbone = traj.topology.select("backbone")
+        traj = traj.superpose(traj[0], atom_indices=backbone)
+    except Exception as e:
+        logging.warning(f"Superposition failed: {e}. Proceeding without superposition.")
+    traj.save(equilibrated_traj.replace(".dcd", "_aligned.dcd"))
+    # os.remove(equilibrated_traj)
+    logging.info(f"Aligned trajectory saved to {equilibrated_traj.replace('.dcd', '_aligned.dcd')}")
+    
+    # Calculate RMSD and RMSF of the ligand
+    u_eq = mda.Universe(system_prmtop, equilibrated_traj.replace(".dcd", "_aligned.dcd"), in_memory=True)
     lig_rmsd_equilibration = compute_rmsd(u_eq, u_eq,
                                           alig_select="backbone", 
                                           groupselections={"ligand":f"resname {lig_resname} and not name H*", 
                                                            "protein":'protein and not name H*'},
-                                          out_dir=f"{sys_name}/equilibration"
+                                          plots_outdir=f"{sys_name}/equilibration"
                                           )
     lig_rmsd_equilibration.to_csv(f"{sys_name}/equilibration/{sys_name}_ligand_rmsd.csv", index=False)
-    plot_atomic_rmsf(u_eq, outname=f"{sys_name}/equilibration/{sys_name}_RMSF.png", log_rmsf=False)
-
-    ########################################################################################
-    ###################################### Steered MD ######################################
-    ########################################################################################
-
-    # equilibrated_pdb = f"{sys_name}/equilibration/{sys_name}_equilibrated.pdb"
-    equilibrated_chk = f"{sys_name}/equilibration/checkpoint_equil_{sys_name}.chk"
-    system_file = f"{sys_name}/equilibration/system_equil_{sys_name}.xml"
-    system_eq = load_system(system_file)
-
-    # load last frame of the equilibrated and WRAPPED trajectory, DO NOT USE THE STRIPPED ONE or the PDB
-    u_eq = mda.Universe(system_prmtop, equilibrated_traj)
-    u_eq.trajectory[-1]  # set pointer to last frame
-
-    # Get pocket atoms
-    pocket_atoms = u_eq.select_atoms("same residue as protein and (around 5 resname UNK) and (name CA C N)")
-    ligand_atoms = u_eq.select_atoms(f"resname {lig_resname} and (not name H*)")
-    restrained_atoms = u_eq.select_atoms("same residue as (protein and around 6 resname UNK) and name CA")
-    
-    restrained_atoms_indices = [atom.index for atom in restrained_atoms]
-    ligand_atoms_indices = [atom.index for atom in ligand_atoms]
-    pocket_atom_indices = [atom.index for atom in pocket_atoms]
-    pocket_full_names = [f"{atom.resname}_{atom.resid}_{atom.index}" for atom in pocket_atoms]
-    # This is to check that the selection is correct
-    # PLEASE debug your own selection 
-    # logging.info(f"Pocket atoms are: {', '.join(set(pocket_full_names))}")
-
-    # Calculate COM distance after equilibration
-    # You can use this to approximate the pulling distance
-    # Additionally, you could use this as a checkpoint and stop here if the ligand has drifted too far
-    eq_com = calculate_com_distance(u_eq, ligand_atoms, pocket_atoms, weighByMass=True)
-    final_com = eq_com.values[-1][0]
-    logging.info(f"COM distance after equilibration is: {final_com:.2f} A")       
-
-    # Run steered MD
-    sMD = SteeredMD(
-        system=system_eq,
-        topology=topo,
-        ligand_atoms=ligand_atoms_indices,
-        pocket_atoms=pocket_atom_indices,
-        restrained_atoms=restrained_atoms_indices,
-        restart_velocities=True,
-        out_dir=f"{sys_name}/sMD",
-    )
-
-    sMD.run(sMD_time=1, #ns
-            displacement=1.5, #nm 
-            pulling_force=1000, #KJ/mol/nm^2
-            replicas=3, 
-            checkpoint_file=equilibrated_chk,
-            do_backwards=False # this is for the reverse pulling
-            )
-    
-    ########################################################################################
-    ###################################### Post-processing #################################
-    ########################################################################################
-    # Load and align the sMD trajectories
-    for i in range(1, 4):
-        traj_file = f"{sys_name}/sMD/trajectory_sMD_replica_{i}_forward.dcd"
-        align_trajectory(
-                        prmtop_file=system_prmtop,
-                        traj_file=traj_file,
-                        out_fname=traj_file.replace(".dcd", "_aligned.dcd"),
-                    )
-        os.remove(traj_file) # remove the dcd
+    plot_atomic_rmsf(u_eq, outname=f"{sys_name}/equilibration/{sys_name}_RMSF.png", log_rmsf=True)
 
     return
 
--- example/equilibration/run_eq.q
+++ example/equilibration/run_eq.q
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH -e sMD.err
-#SBATCH -o sMD.out
+#SBATCH -e sMD-3ptb.err
+#SBATCH -o sMD-3ptb.out
 #SBATCH --gres=gpu#:rtxa6000:1
 #SBATCH --time=3-0
 #SBATCH --partition=alphafold,forli
 #SBATCH --exclude=nodea0110,nodea0111
-#SBATCH --job-name="sMD-2hu4"
+#SBATCH --job-name="sMD-3ptb"
 
 export OPENMM_CUDA_COMPILER=$(which nvcc)
 nvidia-smi
 
 source ~/.bashrc
 micromamba activate autopath3
-python autopath_equilibration.py --rec ../data/2hu4.pdb --lig ../data/2hu4.sdf --protocol equilibration_lig_prot_memb.json\
- # resname and smiles are optional
+python ap_equilibration.py --rec ../data/3ptb.pdb --lig ../data/3ptb.sdf --protocol equilibration_lig_prot_memb.json\
+# resname and smiles are optional
 #  --resname UNK\
 #  --smiles 'CCC(CC)O[C@@H]1C=C(C[C@@H]([C@H]1NC(=O)C)N)C(=O)[O-]'\
