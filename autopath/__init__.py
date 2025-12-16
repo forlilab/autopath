2cec9573b5aa445a3e9e26a7edf2372001364d49
manu <manu.llanos@gmail.com>
2025-12-15 16:27:34 -0800

    expose SteeredMDAnalysis

--- autopath/__init__.py
+++ autopath/__init__.py
@@ -12,11 +12,13 @@ from .steered_md import SteeredMD
 from .metadynamics import MetadynamicsMD
 from .relax_md import RelaxMD
 from .vanilla_md import VanillaMD
+from .analysis_smd import SteeredMDAnalysis
 
 __all__ = [
     "Equilibration",
     "SystemPreparation",
     "SteeredMD",
+    "SteeredMDAnalysis",
     "MetadynamicsMD",
     "RelaxMD",
     "VanillaMD",
